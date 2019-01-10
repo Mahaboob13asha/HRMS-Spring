1326f7160440c35de0ea8d9ef12a2f2ca54cfd5e
RaviRK <RaviRK@Pro-PC>
2019-01-10 10:58:41 +0530

    Added new line in main method

--- human-resource-management-system/src/main/java/com/hrms/hrmsProjectApplication.java
+++ human-resource-management-system/src/main/java/com/hrms/hrmsProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class hrmsProjectApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(hrmsProjectApplication.class, args);
 	}
 
